227ee93653d23f4b076059ee9f7462e1d37f9a50
tehnarenok <ershov.mikhaill.nikolaevich@gmail.com>
2020-09-08 16:55:55 +0300

    edit header

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,19 +8,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const Header = () => {
     let [menuVisible, setMenuVisible] = useState(false)
     let [scrollX, setScrollX] = useState(0)
-
-    if(menuVisible) {
-        let credential = navigator.credentials.create({ publicKey: {
-            challenge: new Uint8Array([117, 61, 252, 231, 191, 241]),
-            rp: { id: "acme.com", name: "ACME Corporation" },
-            user: {
-              id: new Uint8Array([79, 252, 83, 72, 214, 7, 89, 26]),
-              name: "jamiedoe",
-              displayName: "Jamie Doe"
-            },
-            pubKeyCredParams: [ {type: "public-key", alg: -7} ]
-          }});
-    }
     
     console.log(menuVisible)
     return (
